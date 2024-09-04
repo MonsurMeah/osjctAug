a4ffc787c19ea734eafcdb3ceeb8cd83d7afefcf
M.Meah <monsur.meah@outlook.com>
2024-09-04 16:21:25 +0100

    Tuesday - GoogleMaps Shows

---
+++ app/src/main/java/com/example/osjcttwo/UserDetails.java
@@ -0,0 +1,48 @@
+package com.example.osjcttwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+
+public class UserDetails extends AppCompatActivity {
+
+    ImageView imageview3,imageview2;
+    EditText editTextTextEmailAddress, editTextTextPassword;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+
+        imageview2 = findViewById(R.id.imageview2);
+        imageview3 = findViewById(R.id.imageview3);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+        button2 = findViewById(R.id.button2);
+
+        //the main_activity waits for the button to be pressed
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(
+                        getApplicationContext(),
+                        //Default MainActivity3.class
+                        scan_button.class
+
+                );
+
+                startActivity(i);
+
+
+            }
+        });
+    }
+}
